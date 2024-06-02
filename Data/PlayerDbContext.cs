3325fbf4d7370050c1e6ce361b0cf794af0106d5
melih <durmazmelih.ml@gmail.com>
2024-06-02 14:31:31 +0300

    Add project files.

---
+++ Data/PlayerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using İleri_Veri_tabani.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace İleri_Veri_tabani.Data
+{
+    public class PlayerDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=İleriVeriTabani;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace İleri_Veri_tabani.Models
+{
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public required string PlayerName { get; set; }
+        public int PlayerAge { get; set; }
+        public string PlayerPosition { get; set; }
+        public string PlayerSquad { get; set; }
+        public int PlayerMinutesPlayed { get; set; }
+        public float PlayerTouches { get; set; }
+        public float PlayerTackles { get; set; }
+    }
+}
